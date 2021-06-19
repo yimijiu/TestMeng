abb41377734e376530ca66a53903916946af92af
meng <1365803422@qq.com>
2021-06-19 18:29:34 +0800

    igore测试

---
+++ yunjiank/src/main/java/com/yunjiank/TestNG/Igore.java
@@ -0,0 +1,18 @@
+package com.yunjiank.TestNG;
+
+import org.testng.annotations.Test;
+
+public class Igore {
+    @Test
+    public  void  igore1(){
+        System.out.println("igore1执行了");
+    }
+    @Test(enabled = false)
+    public  void  igore2(){
+        System.out.println("igore2执行了");
+    }
+    @Test(enabled = true)
+    public  void  igore3(){
+        System.out.println("igore3执行了");
+    }
+}
